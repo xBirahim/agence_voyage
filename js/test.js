5c163d9329011b13d914b786da23a73ad6ef91a7
Papa Birahim Seye <papabirahimseye@gmail.com>
2020-10-13 12:22:51 +0200

    voila

--- js/test.js
+++ js/test.js
@@ -1,17 +1,53 @@
-let depart
+let depart;
 
-let retour
+let retour;
 
-let adultes = document.getElementById("adultes").value
+let adultes;
 
-let enfants = document.getElementById("enfants")
+let enfants;
 
-function calcul(){
+let prix = 40;
 
-    depart = document.getElementById("depart").value
+function dateDifference() {
 
-    retour = document.getElementById("retour").value
+  depart = new Date(document.getElementById("depart").value).getTime();
+retour = new Date(document.getElementById("retour").value).getTime();
+
+console.log(depart);
+console.log(retour);
+
+numberOfDays = (retour - depart)/(1000*60*60*24);
+  console.log(numberOfDays);
+
+}
 
 
-    console.log(depart-retour)
+function total() {
+    dateDifference();
+  paragrahe = document.getElementById("prix");
+
+  depart = document.getElementById("depart").value;
+
+  retour = document.getElementById("retour").value;
+
+  adultes = document.getElementById("adultes").value;
+
+  enfants = document.getElementById("enfants").value;
+
+  let prix_dejeuner
+
+  if (document.getElementById("breakfast_yes").checked) {
+
+    prix_dejeuner = 12 * adultes + 12 * enfants;
+
+  } else if (document.getElementById("breakfast_no").checked) {
+
+    prix_dejeuner = 0;
+
+  }
+
+  prix_total = adultes * prix + enfants * prix * 0.4 + prix_dejeuner;
+
+  paragrahe.innerHTML = prix_total;
+
 }
