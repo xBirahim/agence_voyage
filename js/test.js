6f0ff0abcbdc6d4b8c0d0e86177ed44a150cb4d9
Papa Birahim Seye <papabirahimseye@gmail.com>
2020-10-13 10:38:05 +0200

    commit de Birahim

---
+++ js/test.js
@@ -0,0 +1,17 @@
+let depart
+
+let retour
+
+let adultes = document.getElementById("adultes").value
+
+let enfants = document.getElementById("enfants")
+
+function calcul(){
+
+    depart = document.getElementById("depart").value
+
+    retour = document.getElementById("retour").value
+
+
+    console.log(depart-retour)
+}
