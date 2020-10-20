2880ff6946638a9629f36e853a334fa7dbaad7bf
theo-pannethier <you@example.com>
2020-10-20 12:03:19 +0200

    js acceuil

---
+++ js/js_header.js
@@ -0,0 +1,17 @@
+window.addEventListener('load',function(){
+
+  var searchbar = document.getElementById("search_bar");
+  searchbar.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("search-button").click();
+      console.log(searchbar.value);
+      ;
+    }
+})
+
+  
+
+
+});
+
