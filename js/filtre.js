c2332c83bf2809a799adf12f6e6c1e15d5df27ff
theo-pannethier <you@example.com>
2020-12-05 18:30:38 +0100

    filltre

---
+++ js/filtre.js
@@ -0,0 +1,16 @@
+function afficher(element, nom) {
+    var x,i,presence
+    presence = 0 ;
+    x = document.getElementsByClassName("filterDiv");
+    
+    for (i = 0 ; i < x.lenght ; i++ ){
+        if (x[i] == nom) {
+            presence = 1 ;
+    }
+    if (presence == 1){
+
+    }
+
+    }
+
+}
