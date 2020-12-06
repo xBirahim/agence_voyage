3ed0f68388a563277ea44a02a48d6f75b92a385c
theo-pannethier <you@example.com>
2020-12-06 14:07:53 +0100

    trieur fini et quasi implemanté

--- js/filtre.js
+++ js/filtre.js
@@ -1,16 +1,41 @@
-function afficher(element, nom) {
-    var x,i,presence
-    presence = 0 ;
-    x = document.getElementsByClassName("filterDiv");
-    
-    for (i = 0 ; i < x.lenght ; i++ ){
-        if (x[i] == nom) {
-            presence = 1 ;
-    }
-    if (presence == 1){
+/* js gerant le trie des produits 
+fait le 06/12/2020
+Theo Pannethier
+*/
+
+function tout() { //fonction qui permet d'afficher tout les produits (pour l'ouverture de la page)
+    var x;
+    x = document.getElementsByClassName("trie"); //on recupere les valeurs dans les elements de classe trie
+    for (i = 0; i < x.length; i++) { // boucle qui parcours x
+        console.log("testfor");
+        var liste = x[i].className.split(" "); //on obtient un liste des parametre de la classe
+        if (liste[liste.length - 1] != "show") { // on verifie que show n'est pas deja present
+            x[i].className = x[i].className + " show"; //on rajoute à la liste show qui permet de l'afficher avec CSS
+        }
 
     }
 
-    }
+}
 
+
+function del(y) { // fonction qui ne concerve que les elements qui possedes y dans leurs classe
+    var x;
+    x = document.getElementsByClassName("trie"); //on recupere les valeurs dans les elements de classe trie
+    for (i = 0; i < x.length; i++) {// boucle qui parcours x
+        var listeTest = x[i].className.split(" ") //liste des parametres de la classe 
+        if (listeTest.includes(y) != true) { // verification de l'inclusion ou non de y dans la classe
+            if (listeTest[listeTest.length - 1] == "show") { //on cache les elements non voulus
+                x[i].className = (x[i].className.split(" show")[0]);
+            }
+
+        }
+        else {
+            if (listeTest[listeTest.length - 1] != "show") { //on affiche les elements voulus
+                console.log("test if");
+                x[i].className = x[i].className + " show";
+            }
+
+        }
+
+    }
 }
